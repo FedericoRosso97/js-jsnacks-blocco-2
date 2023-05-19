b7711a94b3074b52c128073fb39cfb80b9805480
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-19 17:36:08 +0200

    creo ciclo per indovinare numero

---
+++ snak2/script.js
@@ -0,0 +1,7 @@
+let number=Math.floor(Math.random()*100+1);
+console.log(number);
+let guess=parseInt(prompt('indovina il numero'));
+
+while(!(number===guess)){
+     guess=parseInt(prompt('indovina il numero'));
+    }
