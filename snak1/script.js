3df385fbe16c7762d60c1e39f146d31b362fb8ff
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-19 16:52:09 +0200

    fix

--- snak1/script.js
+++ snak1/script.js
@@ -2,14 +2,9 @@ console.log('ciao')
 const array=[];
 let somma=0;
 let stop=false
-let i=0;
-while(i<array.length || stop==false){
-    let numero=prompt('inserisci un numero!');
+while(somma<50){
+    let numero=parseInt(prompt('inserisci un numero!'));
     somma=somma+numero;
     array.push(numero);
     console.log(array);
-    if(somma>50){
-       stop=true
-    }
-    i=i+1;
 }
