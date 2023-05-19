ace94120d5275e77c512afdd22a28b3e679ebf95
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-19 17:49:38 +0200

    num tentativi

--- snak2/script.js
+++ snak2/script.js
@@ -1,6 +1,7 @@
 let number=Math.floor(Math.random()*100+1);
 console.log(number);
 let guess=parseInt(prompt('indovina il numero'));
+let tentativi=[];
 
 while(!(number===guess)){
      guess=parseInt(prompt('indovina il numero'));
@@ -9,4 +10,6 @@ while(!(number===guess)){
      }else{
         console.log('Il numero da indovinare è più basso!')
      }
+     tentativi.push(guess);
     }
+    console.log(tentativi.length+' è numero di tentativi');
