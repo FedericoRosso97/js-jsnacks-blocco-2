25643b00e1db4cd7fa57ba82f02d2d1f2f37dc2d
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-22 12:53:02 +0200

    snack4

--- snak2/script.js
+++ snak2/script.js
@@ -1,15 +1,17 @@
 let number=Math.floor(Math.random()*100+1);
 console.log(number);
-let guess=parseInt(prompt('indovina il numero'));
-let tentativi=[];
+let guess;
+/*let tentativi=[];*/
+let tentativi=0;
 
 while(!(number===guess)){
-     guess=parseInt(prompt('indovina il numero'));
+     guess=parseInt(prompt('indovina il numero tra 1 e 100'));
      if(number>guess){
         console.log('Il numero da indovinare è più alto!')
-     }else{
+     }else if(number<guess){
         console.log('Il numero da indovinare è più basso!')
      }
-     tentativi.push(guess);
+     //tentativi.push(guess);
+     tentativi++;
     }
-    console.log('Indovinato! '+tentativi.length+' è il numero di tentativi');
+    console.log('Indovinato! '+tentativi+' è il numero di tentativi');
---
+++ snak3/script.js
@@ -0,0 +1,10 @@
+//const userNumber=parseInt(prompt('scrivi un numero'));
+let somma=0;
+for(let i=0;i<10;i++){
+   const userNumber=parseInt(prompt('scrivi un numero'));
+     somma=somma+userNumber;
+}
+media=somma/10
+console.log(media,somma)
+
+
